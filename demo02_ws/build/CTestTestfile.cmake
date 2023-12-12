ce36a440ea2f5f8af45b2781f5b306ad75c94915
zhaoxfchn <2659818783@qq.com>
2023-12-12 23:28:26 +0800

    参数服务器

--- demo02_ws/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zhaoxf/ros_learn/demo02_ws/src
-# Build directory: /home/zhaoxf/ros_learn/demo02_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
