daa30c5f741afbe03642cef69cfb731df17cd3bf
zhaoxfchn <2659818783@qq.com>
2023-12-13 17:46:27 +0800

    launch

--- demo01_ws/build/CTestTestfile.cmake
+++ demo01_ws/build/CTestTestfile.cmake
@@ -6,5 +6,6 @@
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
 subdirs("hello_world")
+subdirs("launch_basic")
 subdirs("plumbing_param")
 subdirs("plumbing_server_client")
---
+++ demo01_ws/build/launch_basic/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zhaoxf/ros_learn/demo01_ws/src/launch_basic
+# Build directory: /home/zhaoxf/ros_learn/demo01_ws/build/launch_basic
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
